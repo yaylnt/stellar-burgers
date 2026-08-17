037e951d1f32607c7d6eb03d9bc2ae8ce1687403
Yuliana Galkina <yayltw17@yandex.ru>
2026-08-17 16:43:17 +0500

    fix: добавлен базовый путь для подготовки к деплою

--- webpack.config.js
+++ webpack.config.js
@@ -83,7 +83,8 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '/stellar-burgers/'
   },
   devServer: {
     static: path.join(__dirname, './dist'),
