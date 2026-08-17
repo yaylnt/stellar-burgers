349b07b1f64ca1717055d576ae101b9081ad203c
Yuliana Galkina <yayltw17@yandex.ru>
2026-08-17 17:14:10 +0500

    chore: увеличен таймаут playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -75,6 +75,7 @@ export default defineConfig({
   webServer: {
     command: 'npm run start',
     url: 'http://localhost:4000',
-    reuseExistingServer: !process.env.CI
+    reuseExistingServer: !process.env.CI,
+    timeout: 120_000
   }
 });
