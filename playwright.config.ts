97356200eb7c3de044732740052caafef6a0c078
Yuliana Galkina <yayltw17@yandex.ru>
2026-08-17 17:47:15 +0500

    fix: вернули дефолтный таймаут

--- playwright.config.ts
+++ playwright.config.ts
@@ -75,7 +75,6 @@ export default defineConfig({
   webServer: {
     command: 'npm run start',
     url: 'http://localhost:4000',
-    reuseExistingServer: !process.env.CI,
-    timeout: 120_000
+    reuseExistingServer: !process.env.CI
   }
 });
